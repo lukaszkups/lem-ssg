f48e27ccdcd61d84b7a86c5f1cc5efe96222b7ea
lukaszkups <lukaszkups@gmail.com>
2024-08-04 14:00:20 +0200

    Added type to build output.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   plugins: [
     nodePolyfills(),
+    dts({
+      insertTypesEntry: true,
+    }),
   ],
   build: {
     lib: {
